cb31083fac52b471f30c9f5c748361ca6a700e8f
jwilkinson6179 <JamesW6179@gmail.com>
2020-07-18 13:49:39 -0400

    Simple services added

--- src/main/java/com/prodinv/repositories/ProductRepository.java
+++ src/main/java/com/prodinv/repositories/ProductRepository.java
@@ -2,9 +2,18 @@ package com.prodinv.repositories;
 
 import com.prodinv.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>
 {
+    @Query("SELECT p FROM Product p WHERE p.name = :name")
+    Product findByName(@Param("name") String searchTerm);
+
+    @Query("SELECT p FROM Product p WHERE p.abbreviation = :abbreviation")
+    Product findByAbbr(@Param("abbreviation") String searchTerm);
 }
