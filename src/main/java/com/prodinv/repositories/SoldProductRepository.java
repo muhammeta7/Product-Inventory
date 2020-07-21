2c4937dfb22d0c3f9b66c18c1c2ef21024d2edec
jwilkinson6179 <JamesW6179@gmail.com>
2020-07-21 08:33:59 -0400

    Configuring service for SoldItems

--- src/main/java/com/prodinv/repositories/SoldProductRepository.java
+++ src/main/java/com/prodinv/repositories/SoldProductRepository.java
@@ -4,7 +4,10 @@ import com.prodinv.models.SoldProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SoldProductRepository extends JpaRepository<SoldProduct, Long>
 {
+    public Optional<SoldProduct> findSoldProductsByPurchaser(String purchaser);
 }
