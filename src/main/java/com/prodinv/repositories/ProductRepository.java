4070e983922ca62c564754d346c1b7618c159473
jwilkinson6179 <JamesW6179@gmail.com>
2020-07-26 12:36:26 -0400

    SQL now orders lexographically

--- src/main/java/com/prodinv/repositories/ProductRepository.java
+++ src/main/java/com/prodinv/repositories/ProductRepository.java
@@ -22,9 +22,9 @@ public interface ProductRepository extends JpaRepository<Product, Long>
     @Query("SELECT p FROM Product p WHERE p.category = :category")
     Collection<Product> findByCategory(@Param("category") String searchTerm);
 
-    @Query("SELECT DISTINCT p.category FROM Product p")
+    @Query("SELECT DISTINCT p.category FROM Product p ORDER BY p.category ASC")
     Collection<String> findCategories();
 
-    @Query("SELECT DISTINCT p.abbreviation FROM Product p")
+    @Query("SELECT DISTINCT p.abbreviation FROM Product p ORDER BY p.abbreviation ASC")
     Collection<String> findAbbreviations();
 }
