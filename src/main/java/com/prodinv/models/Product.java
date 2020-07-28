b0212191436b63a7f1b0abf363390d45a7954117
jwilkinson6179 <JamesW6179@gmail.com>
2020-07-28 12:24:27 -0400

    Merging with product

--- src/main/java/com/prodinv/models/Product.java
+++ src/main/java/com/prodinv/models/Product.java
@@ -5,7 +5,7 @@ import javax.validation.constraints.*;
 import java.util.Set;
 
 @Entity
-@Table(name = "products",
+@Table(name = "product",
         uniqueConstraints = {@UniqueConstraint(columnNames = {"name", "abbreviation"})}
 )
 public class Product {
