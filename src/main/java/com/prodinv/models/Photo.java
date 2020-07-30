aa6bf2c971886948be60bcef6f2e0ab4b399caf5
muhammeta7 <muhammeta7@gmail.com>
2020-07-29 21:28:28 -0400

    Add new model for attempt at uploading image to Postman.

---
+++ src/main/java/com/prodinv/models/Photo.java
@@ -0,0 +1,48 @@
+package com.prodinv.models;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "image_table")
+public class Photo {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "picByte", length = 1000)
+    private byte[] picByte;
+
+//    @ManyToOne
+//    private Product product;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public byte[] getPicByte() {
+        return picByte;
+    }
+
+    public void setPicByte(byte[] picByte) {
+        this.picByte = picByte;
+    }
+
+}
