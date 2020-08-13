67b9627401b08374222bf9597859ddbe53036f84
jwilkinson6179 <JamesW6179@gmail.com>
2020-08-13 02:24:33 -0400

    Forgot Repository annotation

--- src/main/java/com/prodinv/repositories/ComponentRepository.java
+++ src/main/java/com/prodinv/repositories/ComponentRepository.java
@@ -3,7 +3,9 @@ package com.prodinv.repositories;
 import com.prodinv.compositekeys.ComponentKey;
 import com.prodinv.models.Component;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ComponentRepository extends JpaRepository<Component, ComponentKey>
 {
 }
